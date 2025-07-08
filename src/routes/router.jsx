a8027e3ebc05e1831727a871d02c10487f92ceca
toimurhasan <2imur.hasan@gmail.com>
2025-07-08 10:38:14 +0600

    basic structure

---
+++ src/routes/router.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+]);
+
+export default router;
